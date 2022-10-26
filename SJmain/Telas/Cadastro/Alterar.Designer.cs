1bc9dbce6c6d00da55764a53d545a3082e4c44e3
Jonathan <rodriguesb.jon@gmail.com>
2022-10-26 12:13:02 -0300

    tela alterar

--- SJmain/Telas/Cadastro/Alterar.Designer.cs
+++ SJmain/Telas/Cadastro/Alterar.Designer.cs
@@ -29,26 +29,87 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.rdbNome = new System.Windows.Forms.RadioButton();
+            this.rdbCPF = new System.Windows.Forms.RadioButton();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Controls.Add(this.txtPesquisa);
+            this.groupBox1.Controls.Add(this.rdbCPF);
+            this.groupBox1.Controls.Add(this.rdbNome);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(231, 38);
+            this.groupBox1.Size = new System.Drawing.Size(314, 66);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Pesquisar Usuário";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(344, 15);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(115, 63);
+            this.btnPesquisar.TabIndex = 1;
+            this.btnPesquisar.Text = "Pesquisar...";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // rdbNome
+            // 
+            this.rdbNome.AutoSize = true;
+            this.rdbNome.Location = new System.Drawing.Point(6, 46);
+            this.rdbNome.Name = "rdbNome";
+            this.rdbNome.Size = new System.Drawing.Size(107, 17);
+            this.rdbNome.TabIndex = 0;
+            this.rdbNome.TabStop = true;
+            this.rdbNome.Text = "Nome de Usuário";
+            this.rdbNome.UseVisualStyleBackColor = true;
+            // 
+            // rdbCPF
+            // 
+            this.rdbCPF.AutoSize = true;
+            this.rdbCPF.Location = new System.Drawing.Point(155, 46);
+            this.rdbCPF.Name = "rdbCPF";
+            this.rdbCPF.Size = new System.Drawing.Size(45, 17);
+            this.rdbCPF.TabIndex = 1;
+            this.rdbCPF.TabStop = true;
+            this.rdbCPF.Text = "CPF";
+            this.rdbCPF.UseVisualStyleBackColor = true;
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(6, 19);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(302, 20);
+            this.txtPesquisa.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(695, 103);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Alterar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnPesquisar);
             this.Controls.Add(this.groupBox1);
             this.Name = "Alterar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Alterar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +117,10 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.RadioButton rdbCPF;
+        private System.Windows.Forms.RadioButton rdbNome;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
