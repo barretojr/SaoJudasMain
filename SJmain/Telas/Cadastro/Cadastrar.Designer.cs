5c4036e035c155410f30299f1f0072604bfb28e0
JONATHAN BARRETO <tecnologiasj03@gmail.com>
2022-10-27 08:30:20 -0300

    Cadastrar alterado

--- SJmain/Telas/Cadastro/Cadastrar.Designer.cs
+++ SJmain/Telas/Cadastro/Cadastrar.Designer.cs
@@ -28,20 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbContabil = new System.Windows.Forms.CheckBox();
+            this.cbFiscal = new System.Windows.Forms.CheckBox();
+            this.cbLogistica = new System.Windows.Forms.CheckBox();
+            this.cbTecnologia = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(243, 23);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar...";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "E-mail:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telefone:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(71, 81);
+            this.maskedTextBox1.Mask = "999.999.999-99";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(87, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(231, 81);
+            this.maskedTextBox2.Mask = "(99)99999-9999";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(87, 20);
+            this.maskedTextBox2.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nome:";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(243, 146);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 11;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(162, 146);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 12;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(22, 146);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 13;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbTecnologia);
+            this.groupBox1.Controls.Add(this.cbLogistica);
+            this.groupBox1.Controls.Add(this.cbFiscal);
+            this.groupBox1.Controls.Add(this.cbContabil);
+            this.groupBox1.Location = new System.Drawing.Point(348, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(141, 141);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Permissões";
+            // 
+            // cbContabil
+            // 
+            this.cbContabil.AutoSize = true;
+            this.cbContabil.Location = new System.Drawing.Point(18, 23);
+            this.cbContabil.Name = "cbContabil";
+            this.cbContabil.Size = new System.Drawing.Size(90, 17);
+            this.cbContabil.TabIndex = 0;
+            this.cbContabil.Text = "Contabilidade";
+            this.cbContabil.UseVisualStyleBackColor = true;
+            // 
+            // cbFiscal
+            // 
+            this.cbFiscal.AutoSize = true;
+            this.cbFiscal.Location = new System.Drawing.Point(18, 46);
+            this.cbFiscal.Name = "cbFiscal";
+            this.cbFiscal.Size = new System.Drawing.Size(53, 17);
+            this.cbFiscal.TabIndex = 1;
+            this.cbFiscal.Text = "Fiscal";
+            this.cbFiscal.UseVisualStyleBackColor = true;
+            // 
+            // cbLogistica
+            // 
+            this.cbLogistica.AutoSize = true;
+            this.cbLogistica.Location = new System.Drawing.Point(18, 69);
+            this.cbLogistica.Name = "cbLogistica";
+            this.cbLogistica.Size = new System.Drawing.Size(70, 17);
+            this.cbLogistica.TabIndex = 2;
+            this.cbLogistica.Text = "Logística";
+            this.cbLogistica.UseVisualStyleBackColor = true;
+            // 
+            // cbTecnologia
+            // 
+            this.cbTecnologia.AutoSize = true;
+            this.cbTecnologia.Location = new System.Drawing.Point(18, 92);
+            this.cbTecnologia.Name = "cbTecnologia";
+            this.cbTecnologia.Size = new System.Drawing.Size(79, 17);
+            this.cbTecnologia.TabIndex = 3;
+            this.cbTecnologia.Text = "Tecnologia";
+            this.cbTecnologia.UseVisualStyleBackColor = true;
             // 
             // Cadastrar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(477, 291);
+            this.ClientSize = new System.Drawing.Size(528, 191);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerificar);
             this.Name = "Cadastrar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastrar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbTecnologia;
+        private System.Windows.Forms.CheckBox cbLogistica;
+        private System.Windows.Forms.CheckBox cbFiscal;
+        private System.Windows.Forms.CheckBox cbContabil;
     }
 }
