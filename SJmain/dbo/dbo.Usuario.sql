daaf931623d5bfea5bc42009af8a7f878626a96f
JONATHAN BARRETO <tecnologiasj03@gmail.com>
2022-11-11 14:40:50 -0300

    att #006

---
+++ SJmain/dbo/dbo.Usuario.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Usuario] (
+    [idusuario]      INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [iddepartamento] INT           NOT NULL,
+    [nome]           VARCHAR (50)  NOT NULL,
+    [nomeusuario]    VARCHAR (20)  NOT NULL,
+    [senha]          VARCHAR (128) NOT NULL,
+    [cpfusuario]     INT           NOT NULL,
+    [telefone]       INT           NOT NULL,
+    [email]          VARCHAR (50)  NOT NULL,
+    CONSTRAINT [iddepartamento] FOREIGN KEY ([iddepartamento]) REFERENCES [dbo].[Departamento] ([iddepartamento])
+);
