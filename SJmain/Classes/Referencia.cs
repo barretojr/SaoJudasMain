2fb7175d729a3f5cf644755d45573638add6d35d
JONATHAN BARRETO <tecnologiasj03@gmail.com>
2022-11-11 15:38:00 -0300

    script BD

--- SJmain/Classes/Referencia.cs
+++ SJmain/Classes/Referencia.cs
@@ -4,7 +4,38 @@
     {
         public Referencia()
         {
-            //contructor
+            //CREATE TABLE [dbo].[Usuario] (
+            //[idusuario] INT IDENTITY(1, 1) NOT NULL,
+
+            //[iddepartamento] INT NOT NULL,
+            //[nome] VARCHAR(50)  NOT NULL,
+            //[nomeusuario]    VARCHAR(20)  NOT NULL,
+            //[senha]          VARCHAR(128) NOT NULL,
+            //[cpfusuario]     INT NOT NULL,
+            //[telefone] INT NOT NULL,
+            //[email] VARCHAR(50)  NOT NULL,
+            //PRIMARY KEY CLUSTERED([idusuario] ASC),
+            //CONSTRAINT[iddepartamento] FOREIGN KEY([iddepartamento]) REFERENCES[dbo].[Departamento]([iddepartamento]));
+
+
+
+
+
+
+
+
+
+
+
+            //CREATE TABLE [dbo].[Departamento] (
+            //[iddepartamento] INT NOT NULL,
+            //[nomedepartamento] VARCHAR(15) NOT NULL,
+            //UNIQUE NONCLUSTERED([iddepartamento] ASC)
+);
+
+
+
+
         }
 
     }
