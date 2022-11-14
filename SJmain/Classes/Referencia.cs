1b88d9c152885d16c0f515441a19965e89e6ad28
JONATHAN BARRETO <tecnologiasj03@gmail.com>
2022-11-14 14:09:18 -0300

    ref att #01

--- SJmain/Classes/Referencia.cs
+++ SJmain/Classes/Referencia.cs
@@ -18,19 +18,18 @@
             //CONSTRAINT[iddepartamento] FOREIGN KEY([iddepartamento]) REFERENCES[dbo].[Departamento]([iddepartamento]));
 
 
-
-
-
-
-
-
-
-
-
             //CREATE TABLE [dbo].[Departamento] (
             //[iddepartamento] INT NOT NULL,
             //[nomedepartamento] VARCHAR(15) NOT NULL,
             //UNIQUE NONCLUSTERED([iddepartamento] ASC));
+
+
+            //INSERT INTO Usuario ( iddepartamento, nome, nomeusuario, senha, cpfusuario, telefone, email) VALUES
+            //(5, 'Jonathan Barreto', 'Jonathan', 'admin', 1234567891, 12341234, 'jonathan@saojudas'),
+            //(1, 'Administrador', 'admin', 'admin', 69696969, 44284428, 'admin@saojudas'),
+            //(5, 'Higor Chagas', 'Higor', 'admin', 123456789, 1123232342, 'higor@saojudas'),
+            //(5, 'Pedro Amoroso', 'Pedro', 'admin', 485748521, 77885566, 'pedro@saojudas'),
+            //(5, 'Wilson Shoity', 'Wilson', 'admin', 158654114, 11955884, 'wilson@saojudas');
 
 
 
