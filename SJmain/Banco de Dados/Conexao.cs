88a2b6c4dddb391b6a47a960d8c2e811715e52fd
JONATHAN BARRETO <tecnologiasj03@gmail.com>
2022-12-28 17:54:19 -0300

    att #27

--- SJmain/Banco de Dados/Conexao.cs
+++ SJmain/Banco de Dados/Conexao.cs
@@ -15,9 +15,7 @@ namespace SJmain.Classes
             con.ConnectionString = "Data Source = 192.168.5.112,49170; Initial Catalog = SJBD; User ID = sa; Password = Alemanha1982;";
             //con.ConnectionString = "jdbc:sqlserver://;serverName=192.168.5.112;port=49170;databaseName=SJBD";
             //con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=sjbd;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            //con.ConnectionString = @"Data Source=http://sjbd.mysql.uhserver.com/;Initial Catalog=sjbd;User Id=gruposaojudas;Password=SJadm@2014;Integrated Security=True;";            
-            //cnn.ConnectionString = "server=sjbd.mysql.uhserver.com;database=sjbd;uid=gruposaojudas;pwd=SJadm@2014;port=3336";
-
+             
         }
         public  SqlConnection Conectar()
         {
