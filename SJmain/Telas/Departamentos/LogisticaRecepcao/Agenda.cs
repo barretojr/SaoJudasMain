ca59e8e6ee6435480975247cb758f546b3ecdd21
HigorChagas <higornchagas@gmail.com>
2023-01-02 13:44:28 -0300

    Implementada classe que cadastra contatos na agenda no banco de dados

--- SJmain/Telas/Departamentos/LogisticaRecepcao/Agenda.cs
+++ SJmain/Telas/Departamentos/LogisticaRecepcao/Agenda.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows.Forms;
+﻿using SJmain.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SJmain.Telas.Departamentos.Logistica
 {
@@ -8,7 +21,36 @@ namespace SJmain.Telas.Departamentos.Logistica
         {
             InitializeComponent();
         }
+        private void btnSalvar_Click_1(object sender, EventArgs e)
+        {
+            Conexao connect = new Conexao();
+            SqlCommand cmd = new SqlCommand();
 
+            try
+            {
+                cmd.Connection = connect.Conectar();
+                cmd.CommandText = "INSERT INTO Agenda (nome, email, endereco, contato)" + "" +
+                    "VALUES(@nome, @email, @endereco, @contato)";
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@contato", txtContato.Text);
 
+                cmd.ExecuteNonQuery();
+
+                connect.Desconectar();
+
+                MessageBox.Show("Contato salvo.");
+
+                txtContato.Text = String.Empty;
+                txtEmail.Text = String.Empty;
+                txtEndereco.Text = String.Empty;
+                txtNome.Text = String.Empty;
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Alguma coisa deu errado. Tente novamente");
+            }
+        }
     }
 }
