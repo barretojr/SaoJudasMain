1fbb086a053206e0f217c476b9075018ac07b3fe
HigorChagas <higornchagas@gmail.com>
2023-01-02 13:24:25 -0300

    Implementado classe para adicionar contatos da agenda no banco de dados

--- SJmain/Telas/Departamentos/LogisticaRecepcao/Agenda.cs
+++ SJmain/Telas/Departamentos/LogisticaRecepcao/Agenda.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows.Forms;
+﻿using SJmain.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SJmain.Telas.Departamentos.Logistica
 {
@@ -9,6 +22,29 @@ namespace SJmain.Telas.Departamentos.Logistica
             InitializeComponent();
         }
 
+        private void btnSalvar_Click(object sender, System.EventArgs e)
+        {
+            Conexao connect = new Conexao();
+            SqlCommand cmd = new SqlCommand();
 
+            try
+            {
+                cmd.Connection = connect.Conectar();
+                cmd.CommandText = "INSERT INTO Agenda (nome, email, endereco, contato)" + "" +
+                    "VALUES(@nome, @email, @endereco, @contato)";
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@contato", txtContato.Text);
+
+                cmd.ExecuteNonQuery();
+
+                connect.Desconectar();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Alguma coisa deu errado. Tente novamente");
+            }
+        }
     }
 }
