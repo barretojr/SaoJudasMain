8d24829eff4cff27d3564f5daa43db7dbdb6c6e4
HigorChagas <higornchagas@gmail.com>
2023-01-02 11:35:59 -0300

    Implementado método que adiciona os contatos da agenda no banco de dados

--- SJmain/Telas/Departamentos/LogisticaRecepcao/Agenda.Designer.cs
+++ SJmain/Telas/Departamentos/LogisticaRecepcao/Agenda.Designer.cs
@@ -123,6 +123,7 @@
             this.btnSalvar.TabIndex = 10;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // btnDeletar
             // 
